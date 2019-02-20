34af08e709e2803279472f1fa016cd23d39b1493
Girts <girts.strazdins@gmail.com>
2019-02-20 19:46:11 +0100

    Step 06: BookController.listBooks returns a list of books

--- Back-End/Spring-Boot-StepByStep/src/main/java/no/ntnu/sysdev/springtutorial/BookController.java
+++ Back-End/Spring-Boot-StepByStep/src/main/java/no/ntnu/sysdev/springtutorial/BookController.java
@@ -3,10 +3,18 @@ package no.ntnu.sysdev.springtutorial;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class BookController {
     @RequestMapping("/books/list")
-    public String listBooks() {
-        return "No books in the register";
+    public List<Book> listBooks() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("J K Rowling", "Harry Potter 1"));
+        books.add(new Book("J K Rowling", "Harry Potter 2"));
+        books.add(new Book("J K Rowling", "Harry Potter 3"));
+        books.add(new Book("J K Rowling", "Harry Potter 4"));
+        return books;
     }
 }
