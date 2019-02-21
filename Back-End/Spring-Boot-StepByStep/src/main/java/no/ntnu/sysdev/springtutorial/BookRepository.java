78ba4e686e5d688d34cf0a0992bdb48d21eb8110
Girts <girts.strazdins@gmail.com>
2019-02-21 10:58:43 +0100

    Step 09: Read books from MySQL database

--- Back-End/Spring-Boot-StepByStep/src/main/java/no/ntnu/sysdev/springtutorial/BookRepository.java
+++ Back-End/Spring-Boot-StepByStep/src/main/java/no/ntnu/sysdev/springtutorial/BookRepository.java
@@ -1,6 +1,9 @@
 package no.ntnu.sysdev.springtutorial;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,12 +11,24 @@ import java.util.List;
 
 @Repository
 public class BookRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public List<Book> findAll() {
         List<Book> books = new ArrayList<>();
-        books.add(new Book("J K Rowling", "Harry Potter 1"));
-        books.add(new Book("J K Rowling", "Harry Potter 2"));
-        books.add(new Book("J K Rowling", "Harry Potter 3"));
-        books.add(new Book("J K Rowling", "Harry Potter 4"));
+        SqlRowSet rs = jdbcTemplate.queryForRowSet("SELECT * FROM books");
+        if (rs.first()) {
+            Book b = new Book(rs.getString("author"), rs.getString("title"));
+            books.add(b);
+            while (rs.next()) {
+                b = new Book(rs.getString("author"), rs.getString("title"));
+                books.add(b);
+            }
+        }
         return books;
     }
 }
