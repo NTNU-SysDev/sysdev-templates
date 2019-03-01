10644719ff1c5f8280e02a96923d2769211d12bb
Girts <girts.strazdins@gmail.com>
2019-03-01 08:54:55 +0100

    Step 22: Insert book data into the template

--- Back-End/Spring-Boot-StepByStep/src/main/java/no/ntnu/sysdev/springtutorial/PageController.java
+++ Back-End/Spring-Boot-StepByStep/src/main/java/no/ntnu/sysdev/springtutorial/PageController.java
@@ -1,12 +1,23 @@
 package no.ntnu.sysdev.springtutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
+    private final BookRepository repository;
+
+    @Autowired
+    public PageController(BookRepository repository) {
+        this.repository = repository;
+    }
+
     @RequestMapping(value = "/app")
-    public String indexPage() {
+    public String indexPage(Model model) {
+        Book firstBook = repository.getFirstBook();
+        model.addAttribute("book", firstBook);
         return "app";
     }
 }
